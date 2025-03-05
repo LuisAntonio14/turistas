830728643ec012ff148b40de0901df327ca1033c
Luis Antonio Velasco <antonioluis6.velasco@gmail.com>
2025-03-05 08:51:32 -0600

    backendPython

--- backend/config.py
+++ backend/config.py
@@ -2,8 +2,9 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    MONGO_URI = os.environ.get('MONGO_URI') or 'mongodb+srv://antonio:1@cluster0.avklr.mongodb.net/turistas?retryWrites=true&w=majority&appName=Cluster0'
     MONGODB_SETTINGS = {
         'db': 'turistas',
-        'host': 'mongodb+srv://antonio:1@cluster0.avklr.mongodb.net/turistas?retryWrites=true&w=majority&appName=Cluster0',
+        'host': MONGO_URI,
         'port': 27017
     }
