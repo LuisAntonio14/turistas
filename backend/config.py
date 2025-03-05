b948b1a5755b46b021491cd69712c813332f2a7b
Luis Antonio Velasco <antonioluis6.velasco@gmail.com>
2025-03-05 13:21:14 -0600

    prueba api

--- backend/config.py
+++ backend/config.py
@@ -5,4 +5,6 @@ class Config:
     MONGO_URI = os.environ.get('MONGO_URI') or 'mongodb+srv://antonio:1@cluster0.avklr.mongodb.net/turistas?retryWrites=true&w=majority&appName=Cluster0'
     MONGODB_SETTINGS = {
         'db': 'turistas',
+        'host': MONGO_URI,
+        'port': 27017
     }
