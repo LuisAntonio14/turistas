f3f22b0f7a083c887041bde41313ad35c6a817df
Luis Antonio Velasco <antonioluis6.velasco@gmail.com>
2025-03-06 22:28:41 -0600

    método get para obtener turistas de tres colecciones

--- backend/app_routes.py
+++ backend/app_routes.py
@@ -77,3 +77,22 @@ def turistas_lacueva():
     else:
         # Si la locación "La cueva" no se encuentra, devolver error 404
         return jsonify({'message': 'Locación "La Cueva" no encontrada'}), 404
+    
+@bp.route('/turistas/presa', methods=['GET'])
+def turistas_presa():
+    # Buscar la locación con el nombre "Santa Cruz"
+    locacion = Locacion.objects(nombre="Presa de Malpaso").first()
+
+    # Si se encuentra la locación
+    if locacion:
+        # Obtener todos los turistas asociados con esa locación
+        turistas = Turista.objects(locacion=locacion.id)
+
+        # Crear una lista con los datos de los turistas encontrados
+        turistas_list = [{"id": str(turista.id), "hora": turista.hora, "fecha":turista.fecha} for turista in turistas]
+
+        # Responder con la lista de turistas
+        return jsonify(turistas_list), 200
+    else:
+        # Si la locación "Presa de Malpaso" no se encuentra, devolver error 404
+        return jsonify({'message': 'Locación "La Cueva" no encontrada'}), 404
